cc86a0af14064fd3b2a9c1230e00d20391358258
= <=>
2020-08-23 12:12:03 +0100

    Fixed logic bug

--- DecimalToBinaryConverter.cs
+++ DecimalToBinaryConverter.cs
@@ -6,50 +6,50 @@ class Solution {
 		Console.ForegroundColor = ConsoleColor.Green;
 		Console.Write("Please input an integer: ");
         int input = Convert.ToInt32(Console.ReadLine());
-        ConvertToBinary(input);
+        var binary = ConvertToBinary(input);
+		
+		Console.ForegroundColor = ConsoleColor.DarkGreen;
+		Console.WriteLine("\nBase 10 = {0}", input);
+		Console.Write("Base 2 = ");
+		
+		foreach(var bit in binary)
+		{
+			Console.Write(bit);
+		}
+		
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.Write("\n\nPress a key to exit: ");
+		Console.ReadKey();
     }
 
-    static void ConvertToBinary(int input){
-		var original = input;
+    static List<int> ConvertToBinary(int n)
+    {
         //CB 2020-08-23 Create list for 0s and 1s
         var binary = new List<int>();
-       
-        do{
-            double copyInput = input;
-            //CB 2020-08-23 Half input
-            input = input/2;
-            copyInput = copyInput/2;
+        //CB 2020-08-23 While n != 0.5
+        
+        do
+        {
+            double value = n;
+            //CB 2020-08-23 Half n
+            n = n/2;
+            value = value/2;
             
             //CB 2020-08-23 If there is a remainder, add 1 to list
-            if (copyInput % 2 == 0.5){
-                binary.Add(1);
+            if (value % 2 == 1)
+            {
+                binary.Add(0);
             }
             
             //CB 2020-08-23 Else, add 0 to the list
-            else{
-                binary.Add(0);
+            else
+            {
+                binary.Add(1);
             }
         }
-        while(input > 0);
+        while(n > 0);
 
-        binary.Reverse();       
-		Console.WriteLine();
-		
-		Console.ForegroundColor = ConsoleColor.DarkGreen;
-		Console.WriteLine("Base 10 = {0}", original);
-		
-		Console.Write("Base 2 = ");
-		
-		for(var i = 0; i < binary.Count; i++)
-		{
-			Console.Write(binary[i]);
-		}
-		
-		Console.WriteLine();
-		Console.WriteLine();
-		
-		Console.ForegroundColor = ConsoleColor.Green;
-		Console.Write("Press a key to exit: ");
-		Console.ReadKey();
+        binary.Reverse();
+        return binary;
     }
 }
